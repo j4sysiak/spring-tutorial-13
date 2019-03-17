6f342cbe836a84de6902a535cc59856f4fbe143c
j4sysiak <j4sysiak@gmail.com>
2019-03-17 22:12:40 +0100

    2

---
+++ spring-tutorial-13/src/main/java/com/caveofprogramming/spring/test/Address.java
@@ -0,0 +1,33 @@
+package com.jaceksysiak.spring.test;
+
+public class Address {
+
+	private String street;
+	private String postcode;
+	 
+	public Address(){}
+	
+	public Address(String street, String postcode) {
+		this.street = street;
+		this.postcode = postcode;
+	}
+
+	public void init(){
+		System.out.println("Bean created: " + this);
+	}
+	
+	
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [street=" + street + ", postcode=" + postcode + "]";
+	}
+ 
+}
